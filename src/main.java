b625e4c722f7066bca8224edf5b09c7bf2589837
Sheinart <sheinart09@gmail.com>
2023-02-16 10:58:51 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+        int ruble = 25_000;
+        int Miles = ruble / 20;
+        System.out.println("Мили: " + Miles);
+    }
+}
+
